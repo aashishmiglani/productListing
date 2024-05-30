b0dcfe0eaf220c8c20a539e8fb744e0dd7dd0fd3
Aashish Miglani <aashishmiglani54@gmail.com>
2024-05-30 14:10:26 +0530

    dddd

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/*/.{html,js,jsx,ts,tsx}', 
+    './public/index.html',
   ],
   theme: {
     extend: {
